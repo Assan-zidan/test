53ea319bbd4374b6dca6d66232ef8ca91ac768e8
Assan-zidan <nsangouassanzidan@gmail.com>
2022-07-11 18:11:14 +0100

    end project

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -2,11 +2,11 @@ import styles from './Input.module.css';
 import { CgSearch } from "react-icons/cg";
 
 const Input = (props) => {
-    const {placeholder, icon, } = props;
+    const {placeholder, icon, type, className} = props;
     return (
         <>
             <div className={styles.input}>
-                <input placeholder={placeholder}/>
+                <input placeholder={placeholder} type={type} className={className}/>
                 {icon ?<CgSearch/> : null}
             </div>
         </>
--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -5,11 +5,17 @@ import styles from './HomePage.module.css'
 import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
 import Input from "../../components/input/Input";
+import Button from '@mui/material/Button';
 
 import {Link} from 'react-router-dom'
-
+import { FaUserCircle } from "react-icons/fa";
 import icon1 from '../../images/icon1.png'
 import icon2 from '../../images/icon2.png'
+import playStore from '../../images/playStore.png'
+import AppStore from '../../images/AppStore.png'
+import woman from "../../images/woman.png";
+import { IoIosArrowDropleft, IoIosArrowDropright } from "react-icons/io";
+import { AiOutlineWarning} from "react-icons/ai";
 
 const HomePage = () => {
     return (
@@ -20,13 +26,17 @@ const HomePage = () => {
         <section className={styles.search}>
           <Container maxWidth="lg">
             <Grid>
-              <Input placeholder="How can we help you" icon="true" />
+              <Input
+                type="text"
+                placeholder="How can we help you"
+                icon="true"
+              />
             </Grid>
           </Container>
         </section>
         <section className={styles.search}>
-          <Container maxWidth="lg">
-            <Grid container spacing={6}>
+          <Container maxWidth="lg" className={styles.home}>
+            <Grid container spacing={8}>
               <Grid item xs={12} sm={6}>
                 <div className={styles.full}>
                   <h1>Full knowledge of all your documents as they go</h1>
@@ -78,9 +88,128 @@ const HomePage = () => {
                     </div>
                   </Grid>
                 </Grid>
+                <div className={styles.woman}>
+                  <div>
+                    <img src={woman} alt="women" />
+                  </div>
+                  <Grid
+                    container
+                    direction="column"
+                    justifyContent="space-between"
+                    alignItems="center"
+                    id={styles.overlay}
+                  >
+                    <Grid
+                      container
+                      direction="row"
+                      justifyContent="flex-start"
+                      alignItems="flex-start"
+                    >
+                      <div className={styles.covid}>
+                        <AiOutlineWarning /> <span>COVID-19 NEWS</span>
+                      </div>
+                    </Grid>
+                    <Grid
+                      container
+                      direction="row"
+                      justifyContent="space-between"
+                      alignItems="center"
+                      className={styles.arrows}
+                    >
+                      <IoIosArrowDropleft />
+                      <IoIosArrowDropright />
+                    </Grid>
+                    <Grid
+                      container
+                      direction="row"
+                      justifyContent="flex-start"
+                      alignItems="flex-start"
+                      className={styles.infected}
+                    >
+                      <p>
+                        Most infected people will develop mild to moderate{" "}
+                        <br />
+                        illness and recover without hospitalization.
+                        <span>
+                          <Link to="#"> Read more</Link>
+                        </span>
+                      </p>
+                    </Grid>
+                  </Grid>
+                </div>
+                <div className={styles.terms}>
+                  <h6>Terms and conditions*</h6>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea{" "}
+                  </p>
+                </div>
               </Grid>
               <Grid item xs={12} sm={6}>
-                les amis
+                <div className={styles.moukiri}>
+                  <Grid
+                    container
+                    direction="column"
+                    justifyContent="space-between"
+                    alignItems="center"
+                    className={styles.block}
+                  >
+                    <div className={styles.connexion}>
+                      <Grid
+                        container
+                        direction="column"
+                        justifyContent="space-between"
+                        alignItems="center"
+                        className={styles.info}
+                      >
+                        <div className={styles.user}>
+                          <FaUserCircle></FaUserCircle>
+                        </div>
+                        <div className={styles.form}>
+                          <Input placeholder="Enter your name" type="text" />
+                          <Input
+                            placeholder="Enter your password"
+                            type="password"
+                          />
+                        </div>
+                        <div className={styles.forgot}>
+                          <p>
+                            <Link to="/">Forgot/Reset Password ?</Link>
+                          </p>
+                        </div>
+                        <div>
+                          <Button variant="contained" className={styles.button}>
+                            Login
+                          </Button>
+                        </div>
+                        <div className={styles.register}>
+                          <p>
+                            Not Registered ? <Link to="/">Register Now </Link>
+                          </p>
+                        </div>
+                      </Grid>
+                    </div>
+                    <div className={styles.store}>
+                      <Grid
+                        container
+                        direction="row"
+                        justifyContent="center"
+                        alignItems="center"
+                      >
+                        <Grid container spacing={2}>
+                          <Grid item xs={12} sm={6}>
+                            <img src={playStore} alt="" />
+                          </Grid>
+                          <Grid item xs={12} sm={6}>
+                            <img src={AppStore} alt="" />
+                          </Grid>
+                        </Grid>
+                      </Grid>
+                    </div>
+                  </Grid>
+                </div>
               </Grid>
             </Grid>
           </Container>
